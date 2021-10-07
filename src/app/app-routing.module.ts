0bfa249213bfe0fa0091499b8936520c84d5e9d2
paoyomira <paoyomira@hotmail.com>
2021-10-07 09:08:01 -0500

    integracion plantilla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  // {
+  //   path: 'cart',
+  //   loadChildren: () => import('./components/cart/cart.component').then(m => m.CartComponent)
+  // },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
